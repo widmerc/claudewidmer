18c75d0dd5ea1721e2ab1f8c816e3375f627fa69
Claude Widmer <claude.widmer@gmx.ch>
2025-03-26 17:43:53 +0100

    about-me aktualisiert

--- src/app/_components/AboutMe.tsx
+++ src/app/_components/AboutMe.tsx
@@ -12,42 +12,40 @@ const AboutMe: React.FC = () => {
 
       {/* Content */}
       <p className="text-base text-center mb-6 text-gray-800 dark:text-white">
-        Diese Webseite ist im Aufbau.        Hier werde ich mehr über mich und meine Projekte teilen. Mein Fokus ist:
+        Ich bin <span className="font-bold">Claude Widmer</span> und arbeite im GIS-Bereich.
+        Privat beschäftige ich mich mit Web-Technologien und anderen Projekten.
+        Diese Webseite ist im Aufbau, hier werde ich mehr über mich und meine Projekte teilen.
       </p>
 
       {/* Skills Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2 max-w-4xl w-full mx-auto">
 
-      <SkillCard
-        backgroundImage="./img/QGIS.png"
-        text="QGIS"
-      />
-            <SkillCard
-        backgroundImage="./img/PyQGIS.png"
-        text="QGIS Plugins"
-      />
         <SkillCard
-        backgroundImage="./img/python.png"
-        text="Python"
-      />
-      
-      <SkillCard
-        backgroundImage="./img/QWC2.png"
-        text="QGisWebClient2"
-      />
+          backgroundImage="./img/QGIS.png"
+          text="QGIS"
+        />
+        <SkillCard
+          backgroundImage="./img/PyQGIS.png"
+          text="QGIS Plugins"
+        />
+        <SkillCard
+          backgroundImage="./img/python.png"
+          text="Python"
+        />
+        <SkillCard
+          backgroundImage="./img/QWC2.png"
+          text="QGisWebClient2"
+        />
+        <SkillCard
+          backgroundImage="./img/QFieldCloud.png"
+          text="QFieldCloud"
+        />
+        <SkillCard
+          backgroundImage="./img/nextjs.png"
+          text="Next.js"
+        />
 
-      <SkillCard
-        backgroundImage="./img/QFieldCloud.png"
-        text="QFieldCloud"
-      />
-
-    <SkillCard
-        backgroundImage="./img/nextjs.png"
-        text="Next.js"
-      />
-
-
-          </div>
+      </div>
 
     </div>
   );
